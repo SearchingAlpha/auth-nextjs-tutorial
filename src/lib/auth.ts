a61a921ea91cb3cbf8f5b9a5520bbfc81d6d7de2
pablomv <morvegpablo@gmail.com>
2025-03-06 21:18:39 +0100

    tutorial: auth basic

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,4 @@
 import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
 
-export const { auth, handlers } = NextAuth({ providers: [] })
+export const { auth, handlers, signIn } = NextAuth({ providers: [GitHub] }) 
