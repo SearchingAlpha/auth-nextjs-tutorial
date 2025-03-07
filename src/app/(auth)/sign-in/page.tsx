465df49405f323f885eea61c8ce951ae31c2fc68
pablomv <morvegpablo@gmail.com>
2025-03-07 21:24:41 +0100

    commit: auth con correo y prisma.db

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,8 +2,9 @@ import { GithubSignIn } from "@/components/github-sign-in";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
-import { auth } from "@/lib/auth";
+import { auth, signIn } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { executeAction } from "@/lib/executeAction";
 
 const Page = async () => {
   const session = await auth();
@@ -29,8 +30,13 @@ const Page = async () => {
       {/* Email/Password Sign In */}
       <form
         className="space-y-4"
-        action={async () => {
+        action={async (formData: FormData) => {
           "use server";
+          await executeAction({
+            actionFn: async () => {
+              await signIn('credentials', formData)
+            }
+          })
         }}
       >
         <Input
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,28 @@
 import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
 import GitHub from "next-auth/providers/github"
+import db from "@/lib/db/db"
 
-export const { auth, handlers, signIn } = NextAuth({ providers: [GitHub] }) 
+export const { auth, handlers, signIn } = NextAuth({ 
+        providers: [
+            GitHub, 
+            Credentials({
+                credentials: {
+                    email: {},
+                    password: {},
+                },
+                authorize: async (credentials) => {
+                    const user = await db.user.findFirst({
+                        where: {
+                            email: credentials.email,
+                            password: credentials.password,
+                        },
+                    });
+                    if (!user) {
+                        throw new Error("Invalid credentials");
+                    }
+                    return user;
+                },
+            })
+        ],
+ }); 
